f1488433cb4344c1c468369e1d1831deece8d855
matjazbevcplume <44820475+MBevc@users.noreply.github.com>
2023-04-05 14:45:59 +0200

    working frontier, but not optimal

--- frontier.py
+++ frontier.py
@@ -28,6 +28,7 @@ class Frontier:
         self.app.route('/new_url', methods=['GET'])(self.new_url)
         self.app.route('/hashes', methods=['GET'])(self.hashes)
         self.app.route('/save_page', methods=['POST'])(self.save_page)
+        self.app.route('/save_domain', methods=['POST'])(self.save_domain)
 
         self.log_stream = StringIO()
         log_handler = logging.StreamHandler(self.log_stream)
@@ -68,11 +69,59 @@ class Frontier:
                     'RETURNING  *) '
                     'INSERT INTO frontier.inprogress_links '
                     'SELECT * FROM selection;')
+        cur.close()
         response = make_response(jsonify(db_response[0][1]), 200)
         return response
 
+    def save_domain(self):
+        input_json = request.get_json()
+        cur = conn.cursor()
+
+        cur.execute('INSERT INTO frontier.domain '
+                    '(domain, robot_txt_content, sitemap_host_content, '
+                    'robot_delay, robot_allowance, sitemap_content_links) '
+                    f"VALUES ( {input_json.get('domain')}, {input_json.get('robot_txt_content')}, "
+                    f" {input_json.get('sitemap_host_content')}, {input_json.get('robot_delay')}, "
+                    f" {input_json.get('robot_allowance')}, {input_json.get('sitemap_content_links')} )")
+
+        cur.execute('INSERT INTO crawldb.site '
+                    '(domain, robots_content, sitemap_content '
+                    f"VALUE ( {input_json.get('domain')}, {input_json.get('robot_txt_content')}, "
+                    f"{input_json.get('sitemap_content_links')} )")
+
+        cur.close()
+
+        response = make_response("Domain saved",200)
+
+        return response
 
     def save_page(self):
+        input_json = request.get_json()
+
+        if input_json.get('pageType') == "HTML":
+            cur = conn.cursor()
+
+            try:
+                cur.execute('SELECT id FROM crawldb.site WHERE '
+                            f"domain = {input_json.get('domain')}")
+                site_id = cur.fetchall()[0]
+
+                cur.execute('INSERT INTO crawldb.page '
+                            '(site_id, page_type_code, url, '
+                            'html_content, http_status_code, '
+                            'accessed_time, hash) '
+                            f"VALUE ( {site_id}, {input_json.get('pageType')}, {input_json.get('pageType')}, "
+                            f"{input_json.get('url')}, {input_json.get('html_content')}, "
+                            f"{input_json.get('httpStatusCode')}, {input_json.get('accessedTime')}, "
+                            f"{input_json.get('hash')} )")
+
+            except Exception as e:
+                self.logger.error(f'Error saving page: {e}\n')
+
+            for link in input_json.get('links'):
+                cur.execute(f"INSERT INTO frontier.waiting_links VALUES ( {link})")
+
+
         pass
 
     def hashes(self):
