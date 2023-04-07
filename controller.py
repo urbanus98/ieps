2dc6d649a69531aa2c812c98e8d15e5565f11499
matjazbevcplume <44820475+MBevc@users.noreply.github.com>
2023-04-07 02:44:21 +0200

    Create controller.py

---
+++ controller.py
@@ -0,0 +1,28 @@
+import requests
+import json
+
+FRONTIER_ENDPOINT="https://172.23.3.4:49500"
+AUTH = ("Crawler1", "&*qRyQ-7dMCX$S9&")
+
+
+def get_new_url():
+    return requests.get(FRONTIER_ENDPOINT+"/new_url",verify=False,auth=AUTH).json()
+
+
+def scrape(json):
+    return requests.post("http://127.0.0.1:5005/scrape", json=json)
+
+def save_page(json):
+    return requests.post(FRONTIER_ENDPOINT + "/new_url", verify=False, auth=AUTH, json=json)
+
+
+if __name__=='__main__':
+    while True:
+        url_json = get_new_url()
+        scrape_dict = {"messages": [url_json['link']]}
+        scrape_result = scrape(scrape_dict)
+        save_page(scrape_result.json())
+    
+
+
+
