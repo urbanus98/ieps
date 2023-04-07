0e4b81ddfbefdc4709ec48ecfbeeaeff40ebb0a2
matjazbevcplume <44820475+MBevc@users.noreply.github.com>
2023-04-07 03:32:25 +0200

    added timeouts

--- controller.py
+++ controller.py
@@ -7,13 +7,13 @@ AUTH = ("Crawler1", "&*qRyQ-7dMCX$S9&")
 
 
 def get_new_url():
-    return requests.get(FRONTIER_ENDPOINT+"/new_url", verify=False, auth=AUTH).json()
+    return requests.get(FRONTIER_ENDPOINT+"/new_url", verify=False, auth=AUTH,timeout=20).json()
 
 def scrape(json):
-    return requests.post("http://127.0.0.1:5005/scrape", json=json)
+    return requests.post("http://127.0.0.1:5005/scrape", json=json,timeout=20)
 
 def save_page(json):
-    return requests.post(FRONTIER_ENDPOINT + "/save_page", verify=False, auth=AUTH, json=json)
+    return requests.post(FRONTIER_ENDPOINT + "/save_page", verify=False, auth=AUTH, json=json,timeout=20)
 
 
 if __name__=='__main__':
