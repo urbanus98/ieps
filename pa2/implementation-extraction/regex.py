32b071bfac588f25f951fd7b6006282a04b5fd44
lovc21 <jakob.dekleva@gmail.com>
2023-05-04 10:53:13 +0200

    regex update fix

--- pa2/implementation-extraction/regex.py
+++ pa2/implementation-extraction/regex.py
@@ -2,105 +2,92 @@ import re
 import json
 
 
+def extract_patterns(html, patterns):
+    return [re.findall(pattern, html) for pattern in patterns]
+
 
 def getOverstock(html):
-    title_pattern = r'<a href=\"http:\/\/www\.overstock\.com\/cgi-bin\/d2\.cgi\?PAGE=PROFRAME&amp;PROD_ID=\d+"><b>(.*?)<\/b><\/a>'
-    list_price_pattern = r"<s>(.*?)</s>"
-    price_pattern = r"<span class=\"bigred\"><b>(.*?)<\/b><\/span>"
-    saving_pattern = r"<span class=\"littleorange\">(\$[\d.,]+)"
-    savingPercent_pattern = r"<span class=\"littleorange\">.*?(\d+%)"
+    patterns = [
+        r'<a href=\"http:\/\/www\.overstock\.com\/cgi-bin\/d2\.cgi\?PAGE=PROFRAME&amp;PROD_ID=\d+\"><b>(.*?)<\/b><\/a>',
+        r"<s>(.*?)</s>",
+        r"<span class=\"bigred\"><b>(.*?)<\/b><\/span>",
+        r"<span class=\"littleorange\">(\$[\d.,]+)",
+        r"<span class=\"littleorange\">.*?(\d+%)",
+        r"<span class=\"normal\">(.*?(?=<br><a href=\"http:\/\/www\.overstock\.com\/cgi-bin\/d2\.cgi\?PAGE=PROFRAME&amp;PROD_ID=))",
+    ]
+    titles, list_prices, prices, savings, saving_percents, contents = (m for m in extract_patterns(html, patterns))
 
-    titles = re.findall(title_pattern, html)
-    list_prices = re.findall(list_price_pattern, html)
-    price = re.findall(price_pattern, html)
-    saving = re.findall(saving_pattern, html)
-    savingPercent = re.findall(savingPercent_pattern, html)
+    print("Titles:", len(titles))
+    print("ListPrices:", len(list_prices))
+    print("Prices:", len(prices))
+    print("Savings:", len(savings))
+    print("SavingPercents:", len(saving_percents))
+    print("Contents:", len(contents))
 
-    content_pattern = r"<span class=\"normal\">(.*?)</span>"
-    contents = re.compile(content_pattern, re.DOTALL)
-    content = re.findall(contents, html)
-    print(content)
+    min_length = min(len(titles), len(list_prices), len(prices), len(savings), len(saving_percents), len(contents))
 
-    print(len(content))
-
-    for i in range(len(titles)):
+    for i in range(min_length):
         output = {
             'Title': titles[i],
             'ListPrice': list_prices[i],
-            'Price': price[i],
-            'Saving': saving[i],
-            'SavingPercent': savingPercent[i],
-            'Content': content[i]
+            'Price': prices[i],
+            'Saving': savings[i],
+            'SavingPercent': saving_percents[i],
+            'Content': contents[i]
         }
         print(json.dumps(output, indent=1, ensure_ascii=False))
 
 
 def getRTV(html):
-    title_pattern = r'<h1.*?>(.*?)<\/h1>'
-    subtitle_pattern = r'<div class=\"subtitle\">(.*)</div>'
-    lead_pattern = r'<p.*?class="lead".*?>(.*?)<\/p>'
-    author_pattern = r'<div *?class=\"author-name\">(.*)</div>'
-    time_pattern = r"<div *?class=\"publish-meta\">\n\t\t(.*)<br>"
-
-    content_patternr = r"<div *?class=\"article-body\">(.*?)<div class=\"gallery\">"
-    content_pattern = re.compile(content_patternr, re.DOTALL)
-
-    # regex_content = r"<div class=\"article-body\">(.*?)<div class=\"gallery\">"
-    # pattern_content = re.compile(regex_content, re.DOTALL)
-    # content = re.findall(pattern_content, html).group(1)
-
-    title_match = re.search(title_pattern, html).group(1)
-    subtitle_match = re.search(subtitle_pattern, html).group(1)
-    lead_match = re.search(lead_pattern, html).group(1)
-    author_match = re.search(author_pattern, html).group(1)
-    time_match = re.search(time_pattern, html).group(1)
-
-    content_matches = re.search(content_pattern, html).group(1)
-
-    # print(content_matches[0])
-    # print(content)
+    patterns = [
+        r'<h1.*?>(.*?)<\/h1>',
+        r'<div class=\"subtitle\">(.*)</div>',
+        r'<p.*?class="lead".*?>(.*?)<\/p>',
+        r'<div *?class=\"author-name\">(.*)</div>',
+        r"<div *?class=\"publish-meta\">\n\t\t(.*)<br>",
+        r"<div *?class=\"article-body\">(.*?)<div class=\"gallery\">",
+    ]
+    title, subtitle, lead, author, time, content = (m[0] if len(m) > 0 else "" for m in extract_patterns(html, patterns))
 
     output = {
-        'title': title_match,
-        'subtitle': subtitle_match,
-        'lead': lead_match,
-        'author': author_match,
-        'time': time_match,
-        'content': content_matches
+        'title': title,
+        'subtitle': subtitle,
+        'lead': lead,
+        'author': author,
+        'time': time,
+        'content': content
     }
-    # print(json.dumps(output, indent=1, ensure_ascii=False))
+    print(json.dumps(output, indent=1, ensure_ascii=False))
 
 
 def getMimovrste(html):
-
-    title_pattern = r'<h1.*?class="detail__title.*?".*?>\n?(.*)\n?.*?<\/h1>'
-    number_pattern = r'<span.*?class="detail-panel-under-title__text.*>\n?\s*Številka:\s*([0-9]*)\n?\s*<\/span>'
-    # desc_pattern = r'<span.*?itemprop="description".*?>\n?(.*)\n?.*?<\/span>'
-    availability_pattern = r'<h3.*?class="availability-box__status availability-box__status--available.*>\n?\s*(.*)\n?\s*<\/h3>'
-    old_price_pattern = r'<span.*class=".*?price__wrap__box__old__tooltip__value"><span>\n?[\s]*([0-9.,]*).*\n?.*<\/span>'
-    final_price_pattern = r'<div.*class=".*?price__wrap__box__final"><span>\n?[\s]*([0-9,.]*).*\n?.*<\/span>'
-    table_params_pattern =  r'<td.*?class="product-parameters__parameter">\n?(.*)\n?.*<\/td>'
-    table_values_pattern =  r'<td.*?class=".*?product-parameters__parameter--value">\n?(.*)\n?.*?\n?.*?\n?.*?<\/td>'
-
-    title_match = re.search(title_pattern, html).group(1).strip()
-    number_match = re.search(number_pattern, html).group(1).strip()
-    # description = re.search(desc_pattern, html).group(1).strip()
-    availability = re.search(availability_pattern, html).group(1).strip()
-    old_price = re.search(old_price_pattern, html).group(1)
-    final_price = re.search(final_price_pattern, html).group(1)
-    table_params = re.findall(table_params_pattern, html)
-    table_values = re.findall(table_values_pattern, html)
+    patterns = [
+        r'<h1.*?class="detail__title.*?".*?>\n?(.*)\n?.*?<\/h1>',
+        r'<span.*?class="detail-panel-under-title__text.*>\n?\s*Številka:\s*([0-9]*)\n?\s*<\/span>',
+        r'<h3.*?class="availability-box__status availability-box__status--available.*>\n?\s*(.*)\n?\s*<\/h3>',
+        r'<span.*class=".*?price__wrap__box__old__tooltip__value"><span>\n?[\s]*([0-9.,]*).*\n?.*<\/span>',
+        r'<div.*class=".*?price__wrap__box__final"><span>\n?[\s]*([0-9,.]*).*\n?.*<\/span>',
+        r'<td.*?class="product-parameters__parameter">\n?(.*)\n?.*<\/td>',
+        r'<td.*?class=".*?product-parameters__parameter--value">\n?(.*)\n?.*?\n?.*?\n?.*?<\/td>',
+    ]
+    title, number, availability, old_price, final_price, table_params, table_values = (m[0] if len(m) > 0 else "" for m in extract_patterns(html, patterns))
+    title = title.strip()
+    number = number.strip()
+    availability = availability.strip()
 
     table = []
-    for i in range(len(table_params)):
+    min_length = min(len(table_params), len(table_values))
+    for i in range(min_length):
         table.append(table_params[i].strip() + ": " + table_values[i].strip())
-        
+
+
     output = {
-        'title': title_match,
-        'number': number_match,
+        'title': title,
+        'number': number,
         'old_price': old_price,
         'final_price': final_price,
         'availability': availability,
         'technical_details': table
     }
     print(json.dumps(output, indent=1, ensure_ascii=False))
+
